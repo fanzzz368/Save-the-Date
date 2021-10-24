a435e62f6c8194e45ccd6d37f1244793fb3201ee
william32brooks <71570404+william32brooks@users.noreply.github.com>
2021-10-24 13:27:20 -0400

    add src folder

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>Save the Date</h1>
+      <p>Let's get started!</p>
       <div className='hero-btns'>
         <Button
           className='btns'
